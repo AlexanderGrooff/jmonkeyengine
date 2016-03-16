ab8c9e120ee496affe2f20bc363eb6364a75714e
Alexander Grooff <alexandergrooff@gmail.com>
2016-03-16 12:20:28 +0100

    Initial structure for TerrainQuadTest

---
+++ jme3-terrain/src/test/java/com/jme3/terrain/geomipmap/TerrainQuadTest.java
@@ -0,0 +1,14 @@
+package com.jme3.terrain.geomipmap;
+
+import org.junit.Test;
+
+public class TerrainQuadTest {
+
+    /**
+     * No NPE or any exception when clear() a new Cinematic
+     */
+    @Test
+    public void testSomething() {
+
+    }
+}
