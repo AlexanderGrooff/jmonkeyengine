925a09fa0b19fca021640f3197f343f0529eb44a
AlexanderGrooff <alexandergrooff@gmail.com>
2016-04-09 22:42:45 +0200

    Added setLod with test

--- jme3-terrain/src/test/java/com/jme3/terrain/geomipmap/UpdatedTerrainPatchTest.java
+++ jme3-terrain/src/test/java/com/jme3/terrain/geomipmap/UpdatedTerrainPatchTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class UpdatedTerrainPatchTest {
+    private final int DIR_RIGHT = 0, DIR_DOWN = 1, DIR_LEFT = 2, DIR_TOP = 3;
+
     @Test
     public void testSetLeftLod() {
         TerrainPatch tp = new TerrainPatch();
@@ -35,5 +37,23 @@ public class UpdatedTerrainPatchTest {
         UpdatedTerrainPatch utp = new UpdatedTerrainPatch(tp);
         utp.setBottomLod(1);
         assertEquals(utp.getBottomLod(), 1);
+    }
+
+    @Test
+    public void testSetLod() {
+        TerrainPatch tp = new TerrainPatch();
+        UpdatedTerrainPatch utp = new UpdatedTerrainPatch(tp);
+
+        utp.setLod(1, DIR_DOWN);
+        assertEquals(utp.getBottomLod(), 1);
+
+        utp.setLod(2, DIR_LEFT);
+        assertEquals(utp.getLeftLod(), 2);
+
+        utp.setLod(3, DIR_RIGHT);
+        assertEquals(utp.getRightLod(), 3);
+
+        utp.setLod(4, DIR_TOP);
+        assertEquals(utp.getTopLod(), 4);
     }
 }
